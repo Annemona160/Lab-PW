ed2baadf27234285cc7fed694328f5b664a21e02
Annemona Gheorghe <annemona.gheorghe@student.unitbv.ro>
2026-03-25 19:04:13 +0200

    Ex5: Stare (state) cu useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
+import { useState } from 'react';
 import Card from './Card';
 function App() {
+    const [count, setCount] = useState(0);
+
     const projects = [
   { title: "Proiect 1", description: "Pagina personala" },
   { title: "Proiect 2", description: "Calculator buget" },
   { title: "Proiect 3", description: "Dashboard React" },
-  { title: "Proiect 4", description: "Magazin Online" }, 
+  { title: "Proiect 4", description: "Magazin Online" }, //Cel adaugat in plus 
 ];
 
  return (
  <div>
  <h1>Laborator PW</h1>
+  <h2>Gheorghe Annemona Gabriela</h2> 
+      <p>Cateva lucruri despre mine:</p>
+          <ul>
+             <li>In prezent-studenta la Calculatoare</li>
+             <li>Sunt pasionata de calatorii</li>
+         </ul>
+         
+         <p>Ai apasat de {count} ori</p>
+         <button onClick={() => setCount(count + 1)}>Click</button>
+        
  {projects.map(function(item, index) {
   return <Card key={index} title={item.title} description={item.description} />;
 })}
+
  </div>
  );
 }
