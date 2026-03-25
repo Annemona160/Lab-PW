4e17c45c2d1070c1c9cc338f51ccd25c7c89569d
Annemona Gheorghe <annemona.gheorghe@student.unitbv.ro>
2026-03-25 17:45:50 +0200

    Ex4: Generare lista cu map()

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Card from './Card';
 function App() {
+    const projects = [
+  { title: "Proiect 1", description: "Pagina personala" },
+  { title: "Proiect 2", description: "Calculator buget" },
+  { title: "Proiect 3", description: "Dashboard React" },
+  { title: "Proiect 4", description: "Magazin Online" }, 
+];
+
  return (
  <div>
  <h1>Laborator PW</h1>
- <Card title="Proiect 1" description="Pagina personala cu HTML si CSS" />
- <Card title="Proiect 2" description="Pagina interactiva cu JavaScript" />
- <Card title="Proiect 3" description="Dashboard cu React" />
- <Card />
+ {projects.map(function(item, index) {
+  return <Card key={index} title={item.title} description={item.description} />;
+})}
  </div>
  );
 }
