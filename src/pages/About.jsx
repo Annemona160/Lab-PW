59cd04ca8e9c58bcd7808edef6cd1d84e12b09cc
Annemona160 <annemona.gheorghe@student.unitbv.ro>
2026-04-28 22:39:29 +0300

    ex5:  Adăugați o pagină nouă singuri

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h2>Despre mine</h2>
+      <p>Salut! Eu sunt creatoarea acestei noi pagini in proiectul meu React.</p>
+    </div>
+  );
+}
+
+export default About;
